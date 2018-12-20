9fe0f124de7b2988a3729314a6849b1576c994be
robbylewis7 <40042762+robbylewis7@users.noreply.github.com>
2018-12-19 17:06:17 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import Landing from './landingPage'
+import Signup from './signup'
+import Login from './login'
+import Main from './main'
+
+
+export default class App extends React.Component {
+  render () {
+
+    return (
+      <div>
+        <Route exact path='/' component={Landing}/>
+        <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/main' component={Main}/>
+      </div>
+    )
+  }
+}
---
+++ top-info.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Pulse from './pulseLogo.png'
+import './top-info.css'
+import { Link } from 'react-router-dom'
+
+
+
+export default class TopInfo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div id = "topInfo">
+
+                <div id = "logo">
+                    <img src={Pulse} id = "frontPageLogo"></img>
+                    <Link to={'/'}>
+                        <div className = "frontPageName">Pulse</div>
+                    </Link>
+                </div>
+
+            <div id = "buttons">
+                <Link to={'/signup'}>
+                    <button className="loginButton">Sign Up</button>
+                </Link>
+
+                <Link to={'/login'}>
+                    <button className="loginButton">Login</button>
+                </Link>
+            </div>
+
+            </div>
+        );
+    }
+}
